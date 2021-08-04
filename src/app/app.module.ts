f774fb2cf9a95abf099362570e1b3c8831a8d0df
kat-dev-coder <72075396+kat-dev-coder@users.noreply.github.com>
2021-08-04 15:44:34 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
     MenuComponent,
-    ImprintComponent
+    ImprintComponent,
+    DataProtectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
